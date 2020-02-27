e25beffbd6fe7889ed926251e84c378c81acce99
Tomáš David <tdavid@redhat.com>
2020-02-27 10:47:19 +0100

    Update SimpleIT.java

--- src/test/java/org/kie/kogito/tooling/SimpleIT.java
+++ src/test/java/org/kie/kogito/tooling/SimpleIT.java
@@ -28,7 +28,7 @@ public class SimpleIT {
     @Test
     public void openGoogleTest() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1920x1080");
+        //chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1920x1080");
 //        chromeOptions.addExtensions(new File("/home/tdavid/Data/Projects/kiegroup/kogito-tooling/packages/chrome-extension-pack-kogito-kie-editors/dist/chrome_extension_kogito_kie_editors_0.2.8/dist.crx"));
         ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
 
