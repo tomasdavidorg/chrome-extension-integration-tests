b10562f5f179c82d08d1ff2c45ca782e1a85d7eb
Tomas David <tdavid@redhat.com>
2020-02-28 14:16:45 +0100

    Add some testing actions

---
+++ src/test/java/org/kie/kogito/tooling/KogitoButtons.java
@@ -0,0 +1,31 @@
+package org.kie.kogito.tooling;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class KogitoButtons {
+
+    @FindBy(xpath = "//button[@data-testid='see-as-diagram-button']")
+    private WebElement seeAsDiagramButton;
+
+    @FindBy(xpath = "//button[@data-testid='open-ext-editor-button']")
+    private WebElement openInOnlineEditorButton;
+
+    @FindBy(xpath = "//button[@data-testid='copy-link-button']")
+    private WebElement copyLinkButton;
+
+    @FindBy(xpath = "//button[@data-testid='see-as-source-button']")
+    private WebElement seeAsSourceButton;
+
+    @FindBy(xpath = "//button[@data-testid='go-fullscreen-button']")
+    private WebElement fullScreenButton;
+
+
+    public void seeAsSource() {
+        seeAsSourceButton.click();
+    }
+
+    public void seeAsDiagram() {
+        seeAsDiagramButton.click();
+    }
+}
--- src/test/java/org/kie/kogito/tooling/SimpleIT.java
+++ src/test/java/org/kie/kogito/tooling/SimpleIT.java
@@ -2,44 +2,47 @@ package org.kie.kogito.tooling;
 
 import java.io.File;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SimpleIT {
 
-    private WebDriver driver;
+    private WebDriver chromeDriver;
 
     @BeforeEach
     public void createDriver() {
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        File kogitoPlugin = new File(getClass().getClassLoader().getResource("dist.crx").getFile());
+        chromeOptions.addExtensions(kogitoPlugin);
+        chromeDriver = new ChromeDriver(chromeOptions);
     }
 
     @AfterEach
     public void teardown() {
-        if (driver != null) {
-            driver.quit();
+        if (chromeDriver != null) {
+            chromeDriver.quit();
         }
     }
 
     @Test
     public void openGoogleTest() throws InterruptedException {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        File kogitoPlugin = new File(getClass().getClassLoader().getResource("dist.crx").getFile());
-        //chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1920x1080");
-        chromeOptions.addExtensions(kogitoPlugin);
-                 ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
-
-//        chromeDriver.set
         chromeDriver.get("https://github.com/kiegroup/kie-wb-playground/blob/master/evaluation/src/main/resources/");
         chromeDriver.findElement(By.linkText("evaluation.bpmn")).click();
 
         new WebDriverWait(chromeDriver, 5);
+
+
 //        ExpectedConditions.alertIsPresent().
 //                new Wait<>()
 //        for (int i = 0; i< 5; i++) {
@@ -50,7 +53,17 @@ public class SimpleIT {
 //        }
 
         Thread.sleep(5000);
-        chromeDriver.findElement(By.className("kogito-button")).click();
+        KogitoButtons kogitoButtons = PageFactory.initElements(chromeDriver, KogitoButtons.class);
+        kogitoButtons.seeAsSource();
+        Assertions.assertThat(chromeDriver.findElement(By.className("kogito-iframe-container")).isDisplayed()).isFalse();
+        Assertions.assertThat(chromeDriver.findElement(By.className("js-file-line-container")).isDisplayed()).isTrue();
+
+        kogitoButtons.seeAsDiagram();
+        Assertions.assertThat(chromeDriver.findElement(By.className("kogito-iframe-container")).isDisplayed()).isTrue();
+        Assertions.assertThat(chromeDriver.findElement(By.className("js-file-line-container")).isDisplayed()).isFalse();
+//        Assertions.assertThat()
+
+       // chromeDriver.findElement(By.className("kogito-button")).click();
 
         Thread.sleep(20000);
     }
